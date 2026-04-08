4e24a24dbee8ad8768757d9500aac629001bc867
Paul Schultz <pschultz@pobox.com>
2026-04-08 18:19:53 -0500

    fix: suppress unused_mut warning on mobile builds

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[allow(unused_mut)]
     let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build());
 

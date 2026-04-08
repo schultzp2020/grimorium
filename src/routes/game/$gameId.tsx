b29f5ff35b8f942cbb9a21eb4df1f759b183b232
Paul Schultz <pschultz@pobox.com>
2026-04-08 13:43:15 -0500

    feat: add /game/$gameId file-based route with validation and redirect

---
+++ src/routes/game/$gameId.tsx
@@ -0,0 +1,37 @@
+import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router'
+import { useEffect } from 'react'
+
+import { GameScreen } from '../../components/screens'
+import { clearCurrentGame, getGame, setCurrentGameId } from '../../lib/storage'
+
+export const Route = createFileRoute('/game/$gameId')({
+  beforeLoad: ({ params }) => {
+    const game = getGame(params.gameId)
+    if (!game) {
+      throw redirect({ to: '/' })
+    }
+    return { hideLanguagePicker: true }
+  },
+  component: GamePage,
+})
+
+function GamePage() {
+  const { gameId } = Route.useParams()
+  const game = getGame(gameId)!
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    setCurrentGameId(gameId)
+  }, [gameId])
+
+  return (
+    <GameScreen
+      key={gameId}
+      initialGame={game}
+      onMainMenu={() => {
+        clearCurrentGame()
+        void navigate({ to: '/' })
+      }}
+    />
+  )
+}

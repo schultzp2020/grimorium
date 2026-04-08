5edb0593915c8f89eab660d352160ee15dfab44e
Paul Schultz <pschultz@pobox.com>
2026-04-08 13:41:51 -0500

    feat: add getInitialMachineState for game state restoration

--- src/lib/machine/index.ts
+++ src/lib/machine/index.ts
@@ -1,3 +1,4 @@
 export { gameMachine } from './gameMachine'
+export { getInitialMachineState } from './getInitialMachineState'
 export { createInitialContext } from './types'
 export type { GameMachineContext, GameMachineEvent } from './types'
